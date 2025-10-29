b57a439e37ca4542f9bf3aacc0c6fbb149862253
WhiteCloudCN <113251172@qq.com>
2025-10-29 13:44:14 +0800

    初始化插件

---
+++ src/main/java/cn/meowyun/mcqlink/Mcqlink.java
@@ -0,0 +1,17 @@
+package cn.meowyun.mcqlink;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Mcqlink extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
